f3f3d9b9ca239b0770eb8e3fea4f7029d2902e3c
Estelle <Dellwolf@pc2.home>
2013-11-10 18:14:16 +0100

    hover best artists / suite

--- js/script2.js
+++ js/script2.js
@@ -1,26 +1,33 @@
 $(document).ready(function(){
 
+	
+	//hover best artist colone de gauche
 	$( "#best-artists-left .onebestartist" ).hover(function() {
     $(this).find('.bartists-container').stop().animate({
 			left: "-350px"
 		},500);
+		$(this).find('.artist-description').stop().fadeIn('slow');
   }, function() {
     $(this).find('.bartists-container').stop().animate({
 			left: "0"
 		},500);
+		$(this).find('.artist-description').stop().fadeOut('slow');
 	}
   );
-  
+  //hover best artist colone de droite
   $( "#best-artists-right .onebestartist" ).hover(function() {
     $(this).find('.bartists-container').stop().animate({
 			right: "-350px"
 		},500);
+		$(this).find('.artist-description').stop().fadeIn('slow');
   }, function() {
     $(this).find('.bartists-container').stop().animate({
 			right: "0"
 		},500);
+		$(this).find('.artist-description').stop().fadeOut('slow');
 	}
   );
+  
 	
 });
 
