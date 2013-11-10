3fcc0fa27a39e8de8af8f48402944ff09854d6c1
Estelle <Dellwolf@pc2.home>
2013-11-10 17:28:36 +0100

    anim contenu bloc best artist

--- js/script2.js
+++ js/script2.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
-	$('.onebestartist').hover(function(){
-		
-	},
-	{
-	});
+
+	$( "#best-artists-left .onebestartist" ).hover(function() {
+    $(this).find('.bartists-container').stop().animate({
+			left: "-350px"
+		},500);
+  }, function() {
+    $(this).find('.bartists-container').stop().animate({
+			left: "0"
+		},500);
+	}
+  );
+  
+  $( "#best-artists-right .onebestartist" ).hover(function() {
+    $(this).find('.bartists-container').stop().animate({
+			right: "-350px"
+		},500);
+  }, function() {
+    $(this).find('.bartists-container').stop().animate({
+			right: "0"
+		},500);
+	}
+  );
+	
 });
+
+
