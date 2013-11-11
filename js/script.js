529da96c53e4cbd965e1b803c104655965a70c1e
bencouronne <benjamincouronne@MacBook-Pro-de-Benjamin-2.local>
2013-11-11 16:02:51 +0100

    Hover Effect (bug décalage)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-$document.ready(function() {
-	
+$(document).ready(function() {
+	$(".artist").hover(function(){
+		$(this).find('.highlight').stop().fadeIn(500),
+		$(this).find('.artist-name').stop().fadeOut(500)
+	}, function(){
+		$(this).find('.highlight').stop().fadeOut(500);
+		$(this).find('.artist-name').stop().fadeIn(500)
+	});
 });
