39603c58935969f7e02dc3311ff405dedba4dbf6
bencouronne <benjamincouronne@MacBook-Pro-de-Benjamin-2.local>
2013-11-11 17:57:15 +0100

    tests

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 	$(".artist").hover(function(){
 		$(this).find('.highlight').stop().fadeIn(500),
-		$(this).find('.artist-name').stop().fadeOut(500)
+		$(this).find('h1').stop().fadeOut(500)
 	}, function(){
 		$(this).find('.highlight').stop().fadeOut(500);
-		$(this).find('.artist-name').stop().fadeIn(500)
+		$(this).find('h1').stop().fadeIn(500)
 	});
 });
