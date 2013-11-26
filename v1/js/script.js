9f5d0f86909339638acd8690063a2f0adf125f00
Rodolphe30 <Rodolphe.Nivelet@gmail.com>
2013-11-26 10:22:18 +0100

    Mise en place du jQuery pour le signUp

--- v1/js/script.js
+++ v1/js/script.js
@@ -6,4 +6,12 @@ $(document).ready(function() {
 		$(this).find('.highlight').stop().fadeOut(500);
 		$(this).find('h1').stop().fadeIn(500)
 	});
+	// PAGE LOGIN
+	$("#signup_container").hide();
+	$("#boutton_sign_up_click").click(function(){
+		var options = {};
+		$(this).fadeOut(500,function(){
+			$("#signup_container").slideDown(500);
+		});
+	});
 });
